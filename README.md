1a85542bd9e568c56ec588f634483f7f973d3881
Jaewon <sorhd134@gmail.com>
2020-12-08 14:18:44 +0900

    12/08

--- README.md
+++ README.md
@@ -8,3 +8,14 @@ Learning React and ES6 by building a movie discovery app.
 - [ ] TV shows
 - [ ] Search
 - [ ] Detail
+
+## API Verbs
+
+- [x] Now playing (Movie)
+- [x] Top rated (TV)
+- [x] Popular (TV, Movie)
+- [x] Upcoming (Movie)
+- [x] Airing today (TV)
+- [x] TV show detail
+- [x] Movie detail
+- [x] Search (Movie, TV)
---
+++ src/Routes/TV/TVContainer.js
@@ -0,0 +1,27 @@
+import React from "react";
+import TVPresenter from "./TVPresenter";
+
+class TVContainer extends React.Component {
+  state = {
+    topRated: null,
+    airingToday: null,
+    popular: null,
+    error: null,
+    loading: true,
+  };
+
+  render() {
+    const { topRated, airingToday, popular, error, loading } = this.state;
+    return (
+      <TVPresenter
+        topRated={topRated}
+        airingToday={airingToday}
+        popular={popular}
+        error={error}
+        loading={loading}
+      />
+    );
+  }
+}
+
+export default TVContainer;
