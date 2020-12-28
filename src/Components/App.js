a59666b1e799bb20830d8925f933e173780913e8
Jaewon <sorhd134@gmail.com>
2020-12-28 15:44:24 +0900

    Update App.js

--- src/Components/App.js
+++ src/Components/App.js
@@ -1,6 +1,6 @@
 import React from "react";
 import Router from "Components/Router";
-import GlobalStyles from "Components/GlobalStyles";
+import GlobalStyles from "Components/globalStyles";
 
 class App extends React.Component {
   render() {
