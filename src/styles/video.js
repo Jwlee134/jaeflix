63a5b1b67669c067a33d84a02da287fdd21030ae
Jaewon <sorhd134@gmail.com>
2020-12-29 12:14:11 +0900

    Modify CSS

--- src/styles/video.js
+++ src/styles/video.js
@@ -1,10 +1,28 @@
 import styled from "styled-components";
 
+export const Name = styled.div`
+  font-size: 18px;
+  font-weight: 500;
+  line-height: 1.3;
+`;
+
 export const Container = styled.div`
   padding: 0px 10px;
   display: grid;
   grid-template-columns: 1fr 1fr;
   grid-gap: 30px;
+  @media screen and (max-width: 600px) {
+    ${Name} {
+      font-size: 16px;
+      font-weight: 400;
+    }
+  }
+  @media screen and (max-width: 500px) {
+    ${Name} {
+      font-size: 12px;
+    }
+    display: block;
+  }
 `;
 
 export const VideoContainer = styled.div`
@@ -15,19 +33,6 @@ export const Img = styled.img`
   width: 100%;
   margin: 15px 0px;
   border-radius: 10px;
-`;
-
-export const Name = styled.div`
-  font-size: 18px;
-  font-weight: 500;
-  line-height: 1.3;
-  @media screen and (max-width: 600px) {
-    font-size: 16px;
-    font-weight: 400;
-  }
-  @media screen and (max-width: 500px) {
-    font-size: 12px;
-  }
 `;
 
 export const NoVideos = styled.div`
